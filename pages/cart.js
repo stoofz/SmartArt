5826f3b1b098f2ff4da8447561cb4f5482fb5f68
Brittany Cowper <brittferren@gmail.com>
2023-08-31 17:38:02 -0500

    link cartItems to line_items for stripe checkout

--- pages/cart.js
+++ pages/cart.js
@@ -1,16 +1,20 @@
+/* eslint-disable camelcase */
+/* eslint-disable func-style */
+/* eslint-disable semi */
 import Link from 'next/link';
 import prisma from 'utils/prisma';
 import { Button, Drawer, IconButton, Badge } from "@mui/material";
 import { AddShoppingCart } from "@mui/material";
 import React, { useState } from 'react';
+import checkout from './checkout';
 
-const Cart = ({  cartItems, productDetails, subtotal  }) => {
+const Cart = ({ cartItems, productDetails, subtotal, lineItems }) => {
   const [cartOpen, setCartOpen] = useState(false);
 
   const getTotalItems = (items) =>
     items.reduce((acc, item) => acc + item.amount, 0);
 
-    
+
   // console.log("LINE 5", productDetails)
   // console.log("LINE 6", cartItems.id)
   return (
@@ -25,8 +29,8 @@ const Cart = ({  cartItems, productDetails, subtotal  }) => {
 
     //   <div className="m-40">
     //  <Drawer anchor="right" open={cartOpen} onClose={() => setCartOpen(false)}>
-       
-      
+
+
     <div className="font-sans w-96 p-5">
 
       <h2>Your Cart</h2>
@@ -41,7 +45,7 @@ const Cart = ({  cartItems, productDetails, subtotal  }) => {
             <div className="flex justify-between">
               <Button
                 size="small"
-               
+
                 variant="contained"
                 style={{
                   backgroundColor: 'lightgray', // Initial background color (gray)
@@ -51,7 +55,7 @@ const Cart = ({  cartItems, productDetails, subtotal  }) => {
                     backgroundColor: 'darkgray', // Background color on hover (darker gray)
                   },
                 }}
-                // onClick={() => removeFromCart(item.id)}
+              // onClick={() => removeFromCart(item.id)}
               >
                 -
               </Button>
@@ -68,27 +72,35 @@ const Cart = ({  cartItems, productDetails, subtotal  }) => {
                     backgroundColor: 'darkgray', // Background color on hover (darker gray)
                   },
                 }}
-                // onClick={() => addToCart(item)}
+              // onClick={() => addToCart(item)}
               >
                 +
               </Button>
             </div>
-         </div>
+          </div>
         </div>
       ))}
       <h2>Total: ${subtotal}</h2>
+      <button type="submit" onClick={(() => {
+        const session = checkout({
+          lineItems
+          // use window.location to redirect to session.url, which is stripe checkout
+        }).then(session => window.location.assign(session.url));
+      })}>Checkout</button>
 
-</div>
-     
+    </div>
+
+
     // </Drawer>
     //   </div>
     // </>
-  )}
+  )
+}
 
 export async function getServerSideProps({ req }) {
   // Get the user ID from auth.
   // const userId = req.user.id; 
-  const userId = 3; 
+  const userId = 3;
 
   try {
     // Query the database to find the user's cart.
@@ -104,6 +116,7 @@ export async function getServerSideProps({ req }) {
     const cartItems = userCart ? userCart.cartItems : [];
     const serializedCartItems = JSON.parse(JSON.stringify(cartItems));
     const productDetails = [];
+    const lineItems = [];
 
     for (const cartItem of serializedCartItems) {
       const product = await prisma.product.findUnique({
@@ -115,7 +128,7 @@ export async function getServerSideProps({ req }) {
       if (product) {
         // Convert price to a plain number
         const price = Number(product.price);
-        
+
         const totalPerProduct = price * cartItem.qty;
 
         console.log("LINE 75", totalPerProduct)
@@ -131,18 +144,35 @@ export async function getServerSideProps({ req }) {
 
     // Calculate subtotal
     const subtotal = productDetails.reduce((acc, item) => acc + item.totalPerProduct, 0);
-   
-  
-   
+
+
+
     // console.log("LINE 80", productDetails)
     // console.log("LINE 90", subtotal)
 
 
-    return { props: { cartItems: serializedCartItems, productDetails, subtotal } };
+    for (const product of productDetails) {
+      lineItems.push({
+        quantity: product.qty,
+        price_data: {
+          currency: "CAD",
+          product_data: {
+            name: product.name,
+            description: product.description || undefined,
+            images: product.image ? [product.image] : [],
+          },
+          unit_amount: product.price,
+        }
+      });
+    }
+    const serializedLineItems = JSON.parse(JSON.stringify(lineItems));
+
+
+    return { props: { cartItems: serializedCartItems, productDetails, subtotal, lineItems: serializedLineItems } };
 
   } catch (error) {
     console.error('Error fetching cart items:', error);
-    return { props: { cartItems: [] } }; 
+    return { props: { cartItems: [] } };
   }
 }
 
