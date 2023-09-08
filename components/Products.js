2ad8ab5a8809331e5a32ec26f4ff18fd42176ae4
Brittany Cowper <brittferren@gmail.com>
2023-09-08 13:48:30 -0500

    update Products card, add images, create Paginate comp

--- components/Products.js
+++ components/Products.js
@@ -18,17 +18,23 @@ import DialogTitle from '@mui/material/DialogTitle';
 import DialogActions from '@mui/material/DialogActions';
 import Dialog from '@mui/material/Dialog';
 import Rating from '@mui/material/Rating';
-import Box from '@mui/material/Box';
-import Backdrop from '@mui/material/Backdrop';
+import { styled } from '@mui/system';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import OpenInFullIcon from '@mui/icons-material/OpenInFull';
+import { useUser } from '@auth0/nextjs-auth0/client';
+import Paginate from './Pagination';
 
 
 const Products = () => {
   const [products, setProducts] = useState([]);
   const [open, setOpen] = useState(false);
   const [clicked, setClicked] = useState(false);
-  const [value, setValue] = useState(2.5);
+  const { user, error, isLoading } = useUser();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [productsPerPage] = useState(20);
 
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
 
   const userId = useSessionId();
 
@@ -44,7 +50,7 @@ const Products = () => {
       }
     };
     getProducts();
-  }, []);
+  }, [currentPage]);
 
   //add logic to add to wishlist
   const handleHeartClick = (productId) => {
@@ -55,127 +61,170 @@ const Products = () => {
     }
   };
 
-  // const handleDialogOpen = (product) => {
-  //   setSelectProduct(product);
-  //   setDialogOpen((prev) => !prev);
-  // };
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#495E48'
+        // light: will be calculated from palette.primary.main,
+        // dark: will be calculated from palette.primary.main,
+        // contrastText: will be calculated to contrast with palette.primary.main
+      },
+      secondary: {
+        main: '#DDB8A6'
+      },
+      warning: {
+        main: '#893F04'
+      },
+      info: {
+        main: '#fff'
+      }
+    },
+  });
 
-  // const handleDialogClose = () => {
-  //   setSelectProduct(null);
-  //   setDialogOpen(false);
-  // };
+  const ExpandIconStyled = styled(Button)`
+    position: absolute;
+    left: 1;
+    right: 0;
+    top: 0;
+    bottom: 1;
+    width: fit-content;
+    height: 40px;
+    visibility: hidden;
+    color: ${theme.palette.info.main}
+  `;
 
-  //fill in HeartIcon on click and add to/remove from Wishlist
-  // const addToWishlist = (id) => {
-  //   if (!clicked) {
-  //     setClicked(true);
-  //     //logic to add to wishlist
-  //   } else {
-  //     setClicked(false);
-  //     //logic to remove from wishlist
-  //   }
-  // }
+  const HeartIconStyled = styled(Button)`
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 1;
+    width: fit-content;
+    visibility: hidden;
+    color: ${theme.palette.info.main}
+    `;
+
+  const CartIconStyled = styled(Button)`
+    position: absolute;
+    left: 1;
+    right: 1;
+    top: 0;
+    bottom: 1;
+    width: fit-content;
+    height: 40px;
+    visibility: hidden;
+    color: ${theme.palette.info.main}
+    `;
+
+  const ContainerStyled = styled("div")`
+    position: relative;
+    &:hover {
+      .icon-button {
+        visibility: visible;
+      }
+    }  
+   }
+   `;
+
+  const DivStyled = styled("div")`
+    display: flex;
+    align-items: flex-end;
+    justify-content: space-between;
+  `;
+
 
   const productList = () => (products.map((product) =>
-    <Card
-      className={"MuiElevatedCard--01"}
-      key={product.id}
-      variant='outlined'
-      sx={{ boxShadow: 2 }}
-      p={4}
-      m={8}
-    >
-      <CardContent className={"MuiCardContent-root"}>
-        <Grid item p={1} m={2}>
-          <Grid container style={{ height: '100%' }}>
-            <Grid container justifyContent="center">
-              <CardMedia
-                className={"MuiCardMedia-root"}
-                sx={{ height: 140 }}
-                image={product.image}
-                title="image alt"
-              />
-            </Grid>
-          </Grid>
-        </Grid>
-
-        <Grid item p={1} m={0}>
-          <Grid container style={{ height: '100%' }} justifyContent="center">
-            <Typography gutterBottom variant="h7" text-align="center">
-              <NextLink
-                href={{
-                  pathname: "/products/[productId]",
-                  query: { productId: product.id },
-                }}
-                passHref
-              >
-                <Link
-                  classname={"MuiCardContent-link"}
-                  overlay
-                  underline="none"
-                >
-                  {product.name}
-                </Link>
-              </NextLink>
-            </Typography>
-          </Grid>
-        </Grid>
-
-        <Grid item>
-          <Grid container style={{ height: '100%' }} justifyContent="center">
-            <Typography variant="h8" text-align="center">
-              ${(product.price / 100).toFixed(2)}
-            </Typography>
-          </Grid>
-        </Grid>
-
-        <Grid item>
-          <Grid container style={{ height: '100%' }} justifyContent="center">
-            <NextLink
-              href={{
-                pathname: "/products/[productId]",
-                query: { productId: product.id },
-              }}
-              passHref
-            >
-              <Link
-                classname={"MuiCardContent-link"}
-                overlay
-                underline="none"
-              >
-                <Rating
-                  id={product.id}
-                  name="simple-controlled"
-                  precision={0.1}
-                  value={value}
-                  onChange={(newValue) => {
-                    setValue(newValue);
-                  }}
-                >
-                </Rating>
-              </Link>
-            </NextLink>
-          </Grid>
-        </Grid>
-
-        <Grid item>
-          <Grid container justifyContent="center">
+    <Grid item xs={12} sm={6} md={4}>
+      <ContainerStyled>
+        <Card sx={{ minWidth: 200, boxShadow: 1 }}
+          key={product.id}
+          variant='outlined'
+        >
+          <CardMedia
+            component="img"
+            image={`./uploads/${product.image}`}
+            alt="work portfolio"
+            sx={{ display: 'block' }}
+          />
+          <CardContent>
             <CardActions>
-              <Button size="small" onClick={() => handleHeartClick(product.id)}>
-                {clicked === product.id ? <FavoriteIcon /> : <FavoriteBorderIcon />}
-              </Button>
-              <Button size="small"
-                onClick={() => handleAddToCart(product.id, userId)}
-              ><AddShoppingCartIcon  /></Button>
-              <Button onClick={() => setOpen(!open)}>
-                <OpenInFullIcon />
-              </Button>
+              <DivStyled>
+                <ExpandIconStyled
+                  variant="text"
+                  className="icon-button"
+                  onClick={() => setOpen(!open)}
+                >
+                  <OpenInFullIcon />
+                </ExpandIconStyled>
+                <Dialog open={open}>
+                  <Button
+                    variant="text"
+                    onClick={() => setOpen(!open)}
+                  >
+                    <CloseIcon />
+                  </Button>
+                  <DialogTitle>
+                    <NextLink
+                      sx={{ color: theme.palette.primary.main }}
+                      href={{
+                        pathname: "/products/[productId]",
+                        query: { productId: product.id },
+                      }}
+                      passHref
+                    >
+                      <Link
+                        overlay
+                        underline="none"
+                        sx={{ color: theme.palette.primary.main }}
+                      >
+                        {product.name}
+                      </Link>
+                    </NextLink>
+                  </DialogTitle>
+                  {product.description}
+                  ${(product.price / 100).toFixed(2)}
+                  <DialogActions>
+                    <Button
+                      variant="text"
+                      className="icon-button"
+                      sx={{ color: theme.palette.primary.main }}
+                      // works fine in Dialog
+                      onClick={() => handleHeartClick(product.id)}
+                    >
+                      {clicked === product.id ? <FavoriteIcon /> : <FavoriteBorderIcon />}
+                    </Button>
+                    <Button
+                      sx={{ color: theme.palette.primary.main }}
+                      variant="text"
+                      className="icon-button"
+                      onClick={() => handleAddToCart(product.id)}
+                    >
+                      <AddShoppingCartIcon />
+                    </Button>
+                  </DialogActions>
+                </Dialog>
+                {/* funny behaviour, can't unclick, and then can't click anything on page */}
+                <HeartIconStyled
+                  variant="text"
+                  className="icon-button"
+                  onClick={() => handleHeartClick(product.id)}
+                >
+                  {clicked === product.id ? <FavoriteIcon /> : <FavoriteBorderIcon />}
+                </HeartIconStyled>
+                <CartIconStyled
+                  variant="text"
+                  className="icon-button"
+                  onClick={() => handleAddToCart(product.idd)}
+                >
+                  <AddShoppingCartIcon />
+                </CartIconStyled>
+              </DivStyled>
               {/* // adjust backdrop to be transparent */}
-              <Dialog open={open}>
-                <Button onClick={() => setOpen(!open)}>
-                  <CloseIcon />
-                </Button>
-                <DialogTitle>
+            </CardActions>
+
+            <Grid item p={1} m={0}>
+              <Grid container style={{ height: '100%' }} justifyContent="center">
+                <Typography gutterBottom variant="h7" text-align="center">
                   <NextLink
                     href={{
                       pathname: "/products/[productId]",
@@ -184,53 +233,69 @@ const Products = () => {
                     passHref
                   >
                     <Link
-                      classname={"MuiCardContent-link"}
                       overlay
                       underline="none"
                     >
                       {product.name}
                     </Link>
                   </NextLink>
-                </DialogTitle>
-                {product.description}
-                ${(product.price / 100).toFixed(2)}
-                <DialogActions>
-                  <Button size="small" onClick={() => handleHeartClick(product.id)}>
-                    {clicked === product.id ? <FavoriteIcon /> : <FavoriteBorderIcon />}
-                  </Button>
-                  <Button size="small" onClick={() => handleAddToCart(product.id, userId)}>
-                    <AddShoppingCartIcon />
-                  </Button>
-                 
-                </DialogActions>
-                
-              </Dialog>
-              
-            </CardActions>
-       
-          </Grid>
-          
-        </Grid>
-        
-      </CardContent>
-      
-    </Card >
-    
+                </Typography>
+              </Grid>
+            </Grid>
+
+            <Grid item>
+              <NextLink
+                href={{
+                  pathname: "/products/[productId]",
+                  query: { productId: product.id },
+                }}
+                passHref
+              >
+                <Link
+                  overlay
+                  underline="none"
+                >
+                  <Rating
+                    id={product.id}
+                    name="read-only"
+                    readOnly
+                    precision={0.1}
+                    value={2.5}
+                  >
+                  </Rating>
+                </Link>
+              </NextLink>
+            </Grid>
+
+            <Grid item>
+              <Grid container style={{ height: '100%' }} justifyContent="center">
+                <Typography variant="h8" text-align="center">
+                  ${(product.price / 100).toFixed(2)}
+                </Typography>
+              </Grid>
+            </Grid>
+
+          </CardContent>
+        </Card >
+      </ContainerStyled>
+    </Grid >
   ))
 
   return (
-    <Grid container
-      align="center"
-      justifyContent="space-evenly"
-      rowSpacing={1}
-      columnSpacing={1}
-      maxWidth={3 / 4}
-      paddingLeft={50}
-      paddingTop={10}
-    >
-      {productList()}
-    </Grid>
+    <ThemeProvider theme={theme}>
+      <Grid container
+        align="center"
+        justify-content="center"
+        maxWidth="75%"
+        paddingLeft="25%"
+        spacing={5}
+      >
+        {productList()}
+        <Paginate count={10} onChange={(e, value) => setCurrentPage(value)} />
+      </Grid>
+    </ThemeProvider>
   );
 }
 
 export default Products;
+
