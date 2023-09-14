61e971e964077a50cf062c38453cad6e4a85cee8
Brittany Cowper <brittferren@gmail.com>
2023-09-14 11:06:38 -0500

    replace carousel images with better quality files

--- components/MyCarousel.js
+++ components/MyCarousel.js
@@ -8,19 +8,50 @@ import Button from '@mui/material/Button';
 import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 import SwipeableViews from 'react-swipeable-views';
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+import { Montserrat } from 'next/font/google';
 import { autoPlay } from 'react-swipeable-views-utils';
 
+const montserrat = Montserrat({
+  weight: '600',
+  subsets: ['latin']
+});
 
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      montserrat
+    ]
+  },
+  palette: {
+    primary: {
+      main: '#324E4B'
+      // light: will be calculated from palette.primary.main,
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+    secondary: {
+      main: '#F5C9C6'
+    },
+    warning: {
+      main: '#893F04'
+    },
+    info: {
+      main: '#fff'
+    }
+  },
+});
 
 const images = [
   {
     label: 'San Francisco – Oakland Bay Bridge, United States',
-    imgPath:'/swiper/backdrop.jpg',
+    imgPath: '/swiper/backdrop.jpg',
   },
   {
     label: 'Bird',
-    imgPath:'/swiper/butterfly.jpg',
+    imgPath: '/swiper/woman.jpg',
   },
   {
     label: 'Bali, Indonesia',
@@ -28,7 +59,11 @@ const images = [
   },
   {
     label: 'Goč, Serbia',
-    imgPath: '/swiper/people.jpg',
+    imgPath: '/swiper/boho-art.jpg',
+  },
+  {
+    label: 'Bright Watercolour',
+    imgPath: '/swiper/watercolor.jpg',
   },
 ];
 
@@ -50,94 +85,96 @@ function SwipeableTextMobileStepper() {
   };
 
   return (
-    <Box sx={{ maxWidth: '100vw', flexGrow: 1 }}>
-     
-      <AutoPlaySwipeableViews
-        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-        index={activeStep}
-        onChangeIndex={handleStepChange}
-        enableMouseEvents
-        interval={5000} 
-      >
-        {images.map((step, index) => (
-          <div key={step.label}>
-            {Math.abs(activeStep - index) <= 2 ? (
-              <Box
-                component="div"
-                sx={{
-                  width: '100%', 
-                  height: '700px', 
-                  display: 'block',
-                  maxWidth: '100%',
-                  objectFit: 'cover',
-                  position: 'relative',
-                  opacity: 0.8, 
-                }}
-              >
-                <img
-                  src={step.imgPath}
-                  alt={step.label}
-                  style={{
-                    width: '100%',
-                    height: '100%',
-                    objectFit: 'cover',
-                  }}
-                />
-                {/* Add text overlay here */}
-                <Typography
-                  variant="h4"
+    <ThemeProvider theme={theme}>
+      <Box sx={{ maxWidth: '100vw', flexGrow: 1 }}>
+
+        <AutoPlaySwipeableViews
+          axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+          index={activeStep}
+          onChangeIndex={handleStepChange}
+          enableMouseEvents
+          interval={5000}
+        >
+          {images.map((step, index) => (
+            <div key={step.label}>
+              {Math.abs(activeStep - index) <= 2 ? (
+                <Box
+                  component="div"
                   sx={{
-                    position: 'absolute',
-                    bottom: '10%', // Adjust the bottom value to position it as desired
-                    left: '50%',
-                    transform: 'translateX(-50%)',
-                    backgroundColor: 'transperent', // Semi-transparent background
-                    color: 'white',
-                    padding: '20px',
-                    fontSize: '70px',
-                    fontWeight: 'bold',
-                    textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
-                    borderRadius: '10px',
-                    whiteSpace: 'nowrap',
+                    width: '100%',
+                    height: '700px',
+                    display: 'block',
+                    maxWidth: '100%',
+                    objectFit: 'cover',
+                    position: 'relative',
+                    opacity: 0.8,
                   }}
                 >
-                  Discover Art You Love
-                </Typography>
-              </Box>
-            ) : null}
-          </div>
-        ))}
-      </AutoPlaySwipeableViews>
-      <MobileStepper
-        steps={maxSteps}
-        position="static"
-        activeStep={activeStep}
-        nextButton={
-          <Button
-            size="small"
-            onClick={handleNext}
-            disabled={activeStep === maxSteps - 1}
-          >
-            Next
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowLeft />
-            ) : (
-              <KeyboardArrowRight />
-            )}
-          </Button>
-        }
-        backButton={
-          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowRight />
-            ) : (
-              <KeyboardArrowLeft />
-            )}
-            Back
-          </Button>
-        }
-      />
-    </Box>
+                  <img
+                    src={step.imgPath}
+                    alt={step.label}
+                    style={{
+                      width: '100%',
+                      height: '100%',
+                      objectFit: 'cover',
+                    }}
+                  />
+                  {/* Add text overlay here */}
+                  <Typography
+                    variant="h4"
+                    sx={{
+                      position: 'absolute',
+                      bottom: '10%', // Adjust the bottom value to position it as desired
+                      left: '50%',
+                      transform: 'translateX(-50%)',
+                      backgroundColor: 'transperent', // Semi-transparent background
+                      color: 'white',
+                      padding: '20px',
+                      fontSize: '70px',
+                      fontWeight: 'bold',
+                      textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
+                      borderRadius: '10px',
+                      whiteSpace: 'nowrap',
+                    }}
+                  >
+                    Discover Art You Love
+                  </Typography>
+                </Box>
+              ) : null}
+            </div>
+          ))}
+        </AutoPlaySwipeableViews>
+        <MobileStepper
+          steps={maxSteps}
+          position="static"
+          activeStep={activeStep}
+          nextButton={
+            <Button
+              size="small"
+              onClick={handleNext}
+              disabled={activeStep === maxSteps - 1}
+            >
+              Next
+              {theme.direction === 'rtl' ? (
+                <KeyboardArrowLeft />
+              ) : (
+                <KeyboardArrowRight />
+              )}
+            </Button>
+          }
+          backButton={
+            <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+              {theme.direction === 'rtl' ? (
+                <KeyboardArrowRight />
+              ) : (
+                <KeyboardArrowLeft />
+              )}
+              Back
+            </Button>
+          }
+        />
+      </Box>
+    </ThemeProvider>
   );
 }
 
--- components/Navigation.js
+++ components/Navigation.js
@@ -258,8 +258,7 @@ export default function Navigation({ sessionId }) {
       <MenuItem>
         {userId &&
           <Typography>
-            Hi Kate
-            {/* Hi, {user.first_name}! */}
+            Hi, {user.first_name}!
           </Typography>
         }
       </MenuItem>
