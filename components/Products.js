cae8e44b874a8e1178f4ba217e9007e5bc4b3f17
Anastasia <z.anastasia11@gmail.com>
2023-10-14 10:29:10 -0700

    fix add to cart toast

--- components/Products.js
+++ components/Products.js
@@ -37,8 +37,7 @@ import { useWishlist } from '@/utils/wishlistContext';
 // import { handleAddToWishlist, showLoginToast } from '@/utils/wishlistFnWithContext';
 import { ToastContainer } from 'react-toastify';
 import { useWishlistFunctions } from '@/utils/wishlistFnWithContext';
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+
 
 
 
@@ -77,19 +76,7 @@ const Products = () => {
       handleAddToCart(productId, userId);
     } else {
       // User is not logged in, show a toast notification
-      // showLoginToast(textToast);
-      toast.success(textToast, {
-        progressStyle: {
-          backgroundColor: '#b1889d', // Set the progress bar color
-        },
-        position: 'top-right',
-        autoClose: 2000,
-        style: {
-          backgroundColor: '#F5C9C6', // Background color
-          color: 'black', // Text color
-        },
-        icon: () => null,
-      });
+      showLoginToast(textToast);
     }
   };
 
